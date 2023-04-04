660389e54c3667213a869bb3e5a3c058c8bf5c1d
shaithea05 <shaithea.alam@gmail.com>
2023-04-04 15:58:50 -0400

    added descriptions to methods in sortBy

---
+++ pa04/routes/sortBy.js
@@ -0,0 +1,25 @@
+// the following sort by methods were created by Ianna and Shaithea
+// they are used to sort the data by the different fields in our database
+
+
+// Sort By Category:
+// This method returns to the same page except the data is 
+// sorted by its category column. 
+
+
+
+// Sort By Amount:
+// This method returns to the same page except the data is 
+// sorted by its amount column. 
+
+
+
+// Sort By Description:
+// This method returns to the same page except the data is 
+// sorted by its description column. 
+
+
+
+// Sort By Date:
+// This method returns to the same page except the data is 
+// sorted by its date column. 
