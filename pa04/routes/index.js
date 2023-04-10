a9792d8f54b6f62bacdd8cb209a47b3a99f8e594
minsung <kmaxsung@gmail.com>
2023-04-10 14:15:31 -0400

    Testing query parameters, completed sort by category

--- pa04/routes/index.js
+++ pa04/routes/index.js
@@ -4,9 +4,13 @@ const Transaction = require("../models/Transaction");
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  res.locals.transactions = await Transaction.find();
-  console.log(await Transaction.find());
+  if (req.query.sortBy == "category") {
+    res.locals.transactions = await Transaction.find({}).sort({ category: 1 });
+  } else {
+    res.locals.transactions = await Transaction.find();
+  }
   res.render("index", { title: "Transactions" });
+  // console.log(transactions);
 });
 
 module.exports = router;
